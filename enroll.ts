98256dfbfda9bb3845bdaecbd46f24989b0ab091
Vitorkm <vitor-kretiska@hotmail.com>
2024-10-09 10:57:00 -0300

    Update import statements in enroll.ts and transfer.ts

--- enroll.ts
+++ enroll.ts
@@ -1,7 +1,7 @@
 import { Connection, Keypair, PublicKey } from "@solana/web3.js"
 import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor"
-import { IDL, WbaPrereq } from "./programs/wba_prereq";
-import wallet from "./dev-wallet.json";
+import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
+import wallet from "./Turbin3-wallet.json";
 
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
@@ -11,7 +11,7 @@ const github = Buffer.from("sanjsol", "utf8");
 
 const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed" });
 
-const program: Program<WbaPrereq> = new Program(IDL, provider);
+const program: Program<Turbin3Prereq> = new Program(IDL, provider);
 
 const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
 const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(enrollment_seeds, program.programId);
